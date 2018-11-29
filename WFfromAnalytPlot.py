60d0e0423819b94b5e38e187cf8ffe7bda095604
Aleksandra <alexandra@dhcp-165-238.physik.uzh.ch>
2018-11-29 10:06:36 +0100

    Correct calc of Wannier Orbitals from analit solution
    Calculate Wannier orbitals only at integer points using fft. The dependance is only in one direction.
    Plot the dependence and fit it with exponential function.

--- WFfromAnalytPlot.py
+++ WFfromAnalytPlot.py
@@ -37,81 +37,73 @@ def maxarray(wf_massive):
     return wf_maxarray, x_max
 
 
-def fitfunc(x, alpha, beta, gamma, delta): # , epsilon
+def fitfunc(x, alpha, beta, gamma):
     """Function for fitting Wannier function"""
-    # return np.log(np.power(x, alpha) * np.exp(-beta * x) + gamma)
     # In logarithmic scale
-    return np.log(np.exp(-beta * x) * np.power(x, alpha) * gamma) #
-    # Power law with logarithmic corrections in log scale
-    # return np.log(np.power(x, alpha) + beta * np.log(x) + gamma)
+    return np.log(np.exp(-beta * x) * np.power(x, alpha) * gamma)  #
+
 
 # with open('WannierLoc.pickle', 'rb') as f:
 #     [WFocc3d, WFval3d, Nx3d] = pickle.load(f)
 #
-# with open('WannierLoc1d.pickle', 'rb') as f:
-#     [WFoccx, WFvalx, WFoccy, WFvaly, WFoccz, WFvalz,
-#      WFoccxy, WFvalxy, WFoccxyz, WFvalxyz,
-#      Nx1d, Nunitcell] = pickle.load(f)
+with open('WannierLoc1d.pickle', 'rb') as f:
+    [WFoccx, Nx1d] = pickle.load(f)
 
 # with open('HybridWannierLoc.pickle', 'rb') as f:
-#     [wf_hybr, n_xhybr, n_hybr_uc] = pickle.load(f)
+#     [wf_hybr, n_xhybr] = pickle.load(f)
 
 
-with open('WannieFromBloch.pickle', 'rb') as f:
-    [Wannier, Nx, x_cell, NG] = pickle.load(f)
+# with open('WannieFromBloch.pickle', 'rb') as f:
+#     [Wannier, Nx, x_cell, NG] = pickle.load(f)
+#
+# Wannier = np.squeeze(Wannier)
+# print(Wannier.shape)
 
-Wannier = np.squeeze(Wannier)
-print(Wannier.shape)
-
-# WFoccx_abs = np.sqrt(np.sum(WFoccx * np.conj(WFoccx), axis=-1))
+WFoccx_abs = np.sqrt(np.sum(WFoccx * np.conj(WFoccx), axis=-1))
 # WFoccz_abs = np.sqrt(np.sum(WFoccz * np.conj(WFoccz), axis=-1))
 # WFoccxy_abs = np.sqrt(np.sum(WFoccxy * np.conj(WFoccxy), axis=-1))
 # WFoccxyz_abs = np.sqrt(np.sum(WFoccxyz * np.conj(WFoccxyz), axis=-1))
 # WFvalx_abs = np.sqrt(np.sum(WFvalx * np.conj(WFvalx), axis=-1))
 
 # WFhybrid_abs = np.sqrt(np.sum(wf_hybr * np.conj(wf_hybr), axis=-1))
-# Nx1d = n_xhybr
-# Nunitcell = n_hybr_uc
-# WF_forplot = np.real(WFhybrid_abs)
 
-Nx1d = Nx
-Nunitcell = x_cell
-WF_forplot = np.abs(Wannier)
+
+# Nx1d = n_xhybr
+WF_forplot = np.real(WFoccx_abs)
 # WFocc_max, coord_max = maxarray(WF_forplot)
 # coord_max = coord_max * Nunitcell / Nx1d
-coord = np.linspace(0, Nx1d, Nx1d) * Nunitcell / Nx1d
+coord = np.linspace(0, Nx1d, Nx1d)
 # WF_max_len = WFocc_max.size
+print(WF_forplot)
 
 # Fitting
-# xcoo = coord_max # [10:]
-# ycoo = np.log(WFocc_max) # [10:])
-# popt, pcov = curve_fit(fitfunc, xcoo, ycoo)#,
-#                        # bounds=([-np.inf, -np.inf, -np.inf],
-#                        #         [np.inf, np.inf, np.inf]))
+xcoo = coord[3:13:2]  # _max
+ycoo = np.log(WF_forplot[3:13:2])  # _max
+popt, pcov = curve_fit(fitfunc, xcoo, ycoo)#,
+                       # bounds=([-np.inf, -np.inf, -np.inf],
+                       #         [np.inf, np.inf, np.inf]))
 
-# print(pcov)
+print(pcov)
 
 matplotlib.rcParams.update({'font.size': 13})
-
+print('Plot start')
 # Check that WFs exponentially decay
 plt.figure(figsize=(8, 6))
-plt.xlabel('$(100)$ direction', size=15)
-plt.ylabel('$|W_{Hybrid}|$', size=15)
+plt.xlabel('z', size=15)  # $(100)$ direction
+plt.ylabel('$|W|$', size=15)  # _{Hybrid}
 # plt.text(14, -2, '$a=1$ - lattice period')
 plt.yscale('log')
-plt.xscale('log')
+# plt.xscale('log')
 # plt.plot(coord_max, WFocc_max, 'k.')
-plt.plot(coord, WF_forplot)
-# plt.plot(coord, np.exp(fitfunc(coord, *popt)), 'r-',
-#          label=r'fit: $\gamma x^{\alpha} e^{-\beta x} + \delta$, '
-#                r'with ''\n'
-#                r'$\alpha=$%5.3f,''\n'
-#                r'$\beta=$%5.3f,''\n'
-#                r'$\gamma=$%5.3f,''\n'
-#                # r'$\delta=$%5.3f,''\n'
-#                r'$\delta=$%5.3f' % tuple(popt))
-# plt.legend()
-plt.show()  
+plt.plot(coord, WF_forplot, 'k.', markersize=7)
+plt.plot(coord[1:], np.exp(fitfunc(coord[1:], *popt)), 'r-',
+         label=r'fit: $\gamma x^{\alpha} e^{-\beta x}$, '
+               r'with ''\n'
+               r'$\alpha=$%5.3f,''\n'
+               r'$\beta=$%5.3f,''\n'
+               r'$\gamma=$%5.3f,''\n' % tuple(popt))
+plt.legend()
+plt.show()
 
 
 # Draw the contours of Wannier orbitals
--- WFfromAnalytSolution.py
+++ WFfromAnalytSolution.py
@@ -8,214 +8,127 @@ Created on Wed Sep  5 18:36:26 2018
 import numpy as np
 from math import pi
 import pickle
-import scipy.linalg
 
 """
 Construct Wannier Functions from initial eigenstate of Hopf Hamiltonian
 """
 
 
-def fourtransf_occ(xx, yy, zz):
+def fourtransf_occ(nkx, nky, nkz):
     """
     Fourier transformation of two components of occupied Bloch function
     correspond to two localized Wannier functions.
     """
-    wf_occ = np.empty((2, 1), dtype=complex)
-    u2 = - np.multiply(np.divide(np.sin(kkx) - 1j * t * np.sin(kky), np.sqrt(
-        np.power(np.sin(kkx), 2) + t ** 2 * np.power(np.sin(kky), 2) +
-        np.power(np.sin(kkz), 2) +
-        np.power(np.cos(kkx) + np.cos(kky) + np.cos(kkz) + h, 2))),
-                       np.exp(1j * (kkx * xx + kky * yy + kkz * zz)))
+    wf_occ = np.empty((nkx, nky, nkz, 2), dtype=complex)
 
-    u1 = np.multiply(np.divide(np.sin(kkz) -
-                               1j * (np.cos(kkx) + np.cos(kky) +
-                                     np.cos(kkz) + h),
-                               np.sqrt(
-                                   np.power(np.sin(kkx), 2) + t ** 2 * np.power(
-                                       np.sin(kky), 2) +
-                                   np.power(np.sin(kkz), 2) +
-                                   np.power(np.cos(kkx) + np.cos(kky) + np.cos(
-                                       kkz) + h, 2))),
-                     np.exp(1j * (kkx * xx + kky * yy + kkz * zz)))
+    kx = np.linspace(0, 2 * pi, nkx)
+    ky = np.linspace(0, 2 * pi, nky)
+    kz = np.linspace(0, 2 * pi, nkz)
 
-    ufourier = np.fft.fftn(u1) / Nk ** 3
-    wf_occ[0] = ufourier[0, 0, 0]  # Consider only WF(R=0)
-    ufourier = np.fft.fftn(u2) / Nk ** 3
-    wf_occ[1] = ufourier[0, 0, 0]
-    return np.squeeze(wf_occ)
+    # Cartesian coordinates, indexing for correct order x,y,z (not y,x,z)
+    [kkx, kky, kkz] = np.meshgrid(kx, ky, kz, indexing='ij')
+
+    u2 = - (np.divide(np.sin(kkx) - 1j * t * np.sin(kky), np.sqrt(
+            np.power(np.sin(kkx), 2) + t ** 2 * np.power(np.sin(kky), 2)
+            + np.power(np.sin(kkz), 2)
+            + np.power(np.cos(kkx) + np.cos(kky) + np.cos(kkz) + h, 2))))
+
+    u1 = (np.divide(np.sin(kkz) - 1j * (np.cos(kkx) + np.cos(kky)
+          + np.cos(kkz) + h), np.sqrt(
+          np.power(np.sin(kkx), 2) + t ** 2 * np.power(np.sin(kky), 2)
+          + np.power(np.sin(kkz), 2)
+          + np.power(np.cos(kkx) + np.cos(kky) + np.cos(kkz) + h, 2))))
+
+    wf_occ[:, :, :, 0] = np.fft.fftn(u1) / nkx / nky / nkz
+    wf_occ[:, :, :, 1] = np.fft.fftn(u2) / nkx / nky / nkz
+    return wf_occ
 
 
-def fourtransf_val(xx, yy, zz):
+def fourtransf_val(nkx, nky, nkz):
     """
     Fourier transformation of two components of valence Bloch function
     correspond to two localized Wannier functions.
     """
-    wf_val = np.empty((2, 1), dtype=complex)
-    u1 = np.multiply(np.divide(np.sin(kkx) + 1j * t * np.sin(kky), np.sqrt(
-        np.power(np.sin(kkx), 2) + t ** 2 * np.power(np.sin(kky), 2) +
-        np.power(np.sin(kkz), 2) +
-        np.power(np.cos(kkx) + np.cos(kky) + np.cos(kkz) + h, 2))),
-                     np.exp(1j * (kkx * xx + kky * yy + kkz * zz)))
+    wf_val = np.empty((nkx, nky, nkz, 2), dtype=complex)
 
-    u2 = np.multiply(np.divide(np.sin(kkz) +
-                               1j * (np.cos(kkx) + np.cos(kky) +
-                                     np.cos(kkz) + h),
-                               np.sqrt(
-                                   np.power(np.sin(kkx), 2) + t ** 2 * np.power(
-                                       np.sin(kky), 2) +
-                                   np.power(np.sin(kkz), 2) +
-                                   np.power(np.cos(kkx) + np.cos(kky) + np.cos(
-                                       kkz) + h, 2))),
-                     np.exp(1j * (kkx * xx + kky * yy + kkz * zz)))
+    kx = np.linspace(0, 2 * pi, nkx)
+    ky = np.linspace(0, 2 * pi, nky)
+    kz = np.linspace(0, 2 * pi, nkz)
 
-    ufourier = np.fft.fftn(u1) / Nk ** 3
-    wf_val[0] = ufourier[0, 0, 0]  # Consider only WF(R=0)
-    ufourier = np.fft.fftn(u2) / Nk ** 3
-    wf_val[1] = ufourier[0, 0, 0]
-    return np.squeeze(wf_val)
+    # Cartesian coordinates, indexing for correct order x,y,z (not y,x,z)
+    [kkx, kky, kkz] = np.meshgrid(kx, ky, kz, indexing='ij')
+
+    u1 = (np.divide(np.sin(kkx) + 1j * t * np.sin(kky), np.sqrt(
+          np.power(np.sin(kkx), 2) + t ** 2 * np.power(np.sin(kky), 2)
+          + np.power(np.sin(kkz), 2)
+          + np.power(np.cos(kkx) + np.cos(kky) + np.cos(kkz) + h, 2))))
+
+    u2 = (np.divide(np.sin(kkz) + 1j * (np.cos(kkx) + np.cos(kky)
+          + np.cos(kkz) + h), np.sqrt(
+          np.power(np.sin(kkx), 2) + t ** 2 * np.power(np.sin(kky), 2)
+          + np.power(np.sin(kkz), 2)
+          + np.power(np.cos(kkx) + np.cos(kky) + np.cos(kkz) + h, 2))))
+
+    wf_val[:, :, :, 0] = np.fft.fftn(u1) / nkx / nky / nkz
+    wf_val[:, :, :, 1] = np.fft.fftn(u2) / nkx / nky / nkz
+    return wf_val
 
 
-def fourtransf_occ_1d(xx):
+def fourtransf_occ_1d(nk):
     """
     Calculate Hybrid WF, make only 1d Fourier transform
     """
+    wf_occ_1d = np.empty((nk, 2), dtype=complex)
+    kx0 = np.linspace(0, 2 * pi, nk)
     ky0 = 0
     kz0 = 0
-    wf_occ_1d = np.empty((2, 1), dtype=complex)
-    u2 = - np.multiply(np.divide(np.sin(kx) - 1j * t * np.sin(ky0), np.sqrt(
-        np.power(np.sin(kx), 2) + t ** 2 * np.power(np.sin(ky0), 2) +
-        np.power(np.sin(kz0), 2) +
-        np.power(np.cos(kx) + np.cos(ky0) + np.cos(kz0) + h, 2))),
-        np.exp(1j * kx * xx))
 
-    u1 = np.multiply(np.divide((np.sin(kz0) - 1j * (
-        np.cos(kx) + np.cos(ky0) + np.cos(kz0) + h)), np.sqrt(
-        np.power(np.sin(kx), 2) + t ** 2 * np.power(np.sin(ky0), 2) +
-        np.power(np.sin(kz0), 2) +
-        np.power(np.cos(kx) + np.cos(ky0) + np.cos(kz0) + h, 2))),
-        np.exp(1j * kx * xx))
+    u2 = - (np.divide(np.sin(kx0) - 1j * t * np.sin(ky0), np.sqrt(
+            np.power(np.sin(kx0), 2) + t ** 2 * np.power(np.sin(ky0), 2)
+            + np.power(np.sin(kz0), 2)
+            + np.power(np.cos(kx0) + np.cos(ky0) + np.cos(kz0) + h, 2))))
 
-    # ufourier = np.fft.fft(u1) / Nk
-    # wf_occ_1d[0] = ufourier[Nk//2 - 1]  # Consider only WF(R=0)
-    # ufourier = np.fft.fft(u2) / Nk
-    # wf_occ_1d[1] = ufourier[Nk//2 - 1]
-    wf_occ_1d[0] = np.sum(u1) / Nk
-    wf_occ_1d[1] = np.sum(u2) / Nk
-    return np.squeeze(wf_occ_1d)
+    u1 = (np.divide((np.sin(kz0) - 1j * (np.cos(kx0) + np.cos(ky0)
+          + np.cos(kz0) + h)), np.sqrt(
+          np.power(np.sin(kx0), 2) + t ** 2 * np.power(np.sin(ky0), 2)
+          + np.power(np.sin(kz0), 2)
+          + np.power(np.cos(kx0) + np.cos(ky0) + np.cos(kz0) + h, 2))))
+
+    wf_occ_1d[:, 0] = np.fft.fft(u1) / nk
+    wf_occ_1d[:, 1] = np.fft.fft(u2) / nk
+    return wf_occ_1d
 
 
-def scalar_mult(a, b):
-    """Define the scalar product of two vectors"""
-    mult = np.sum(np.conj(a) * b)
-    return mult
-
-
-def normalize(s_overlap, a_vec):
-    """
-    Normalization of the vector a by overlap metrix S
-    a -> S^(-1/2)a
-    """
-    a_vec = np.dot(scipy.linalg.sqrtm(scipy.linalg.inv(s_overlap)), a_vec)
-    return a_vec
-
-
-def wannier_1d(n_x1d, n_unitcell):
+def wannier_1d(n_1d, nkx, nky, nkz):
     """Calculate Wannier function depending on 1 varible (others are 0)"""
-    # The function of only one variable, e.g. (x)
-    # (100)
-    WFoccx = np.empty((n_x1d, 2), dtype=complex)
-    WFvalx = np.empty((n_x1d, 2), dtype=complex)
-    # (010)
-    WFoccy = np.empty((n_x1d, 2), dtype=complex)
-    WFvaly = np.empty((n_x1d, 2), dtype=complex)
-    # (001)
-    WFoccz = np.empty((n_x1d, 2), dtype=complex)
-    WFvalz = np.empty((n_x1d, 2), dtype=complex)
-    # (110) direction
-    WFoccxy = np.empty((n_x1d, 2), dtype=complex)
-    WFvalxy = np.empty((n_x1d, 2), dtype=complex)
-    # (111) direction
-    WFoccxyz = np.empty((n_x1d, 2), dtype=complex)
-    WFvalxyz = np.empty((n_x1d, 2), dtype=complex)
-
-    # For different 1d directions calculate WFs as a Fourier transform of
-    # Bloch solutions
-
-    for nx in range(n_x1d):
-        x = nx * n_unitcell / n_x1d
-        print('nx=', nx)
-        WFoccx[nx, :] = fourtransf_occ(x, 0, 0)
-        # WFvalx[nx, :] = fourtransf_val(x, 0, 0)
-        # WFoccy[nx, :] = fourtransf_occ(0, x, 0)
-        # WFvaly[nx, :] = fourtransf_val(0, x, 0)
-        # WFoccz[nx, :] = fourtransf_occ(0, 0, x)
-        # WFvalz[nx, :] = fourtransf_val(0, 0, x)
-
-        # WFoccxy[nx, :] = fourtransf_occ(x, x, 0)
-        # WFvalxy[nx, :] = fourtransf_val(x, x, 0)
-
-        # WFoccxyz[nx, :] = fourtransf_occ(x, x, x)
-        # WFvalxyz[nx, :] = fourtransf_val(x, x, x)
+    # The function of only one variable, e.g. (x) (100)
+    wf_occ_x = fourtransf_occ(nkx, nky, nkz)[0, 0, 0:n_1d, :]
 
     with open('WannierLoc1d.pickle', 'wb') as f:
-        pickle.dump([WFoccx, WFvalx, WFoccy, WFvaly, WFoccz, WFvalz,
-                     WFoccxy, WFvalxy, WFoccxyz, WFvalxyz,
-                     n_x1d, n_unitcell], f)
+        pickle.dump([wf_occ_x, n_1d], f)
 
 
-def wannier_3d(n_x3d):
-    """Calculate Wannier function depending on 3 variable (x, y, z)"""
-    # Set the size of WF matrices - number of points (x, y, z)
-    # The 3d function of (x, y, z)
-    wf_occ3d = np.empty((n_x3d, n_x3d, n_x3d, 2), dtype=complex)
-    wf_val3d = np.empty((n_x3d, n_x3d, n_x3d, 2), dtype=complex)
-
-    # For (x, y, z) in 4 BZs calculate Wannier functions
-    # as a Fourier transform of analytical Bloch solutions
-
-    for nx in range(n_x3d):
-        x = (nx - (n_x3d - 1) / 2) / (n_x3d-1) * 4
-        print('nx=', nx)
-        for ny in range(n_x3d):
-            y = (ny - (n_x3d - 1) / 2) / (n_x3d-1) * 4
-            print('ny=', ny)
-            for nz in range(n_x3d):
-                z = (nz - (n_x3d - 1) / 2) / (n_x3d-1) * 4
-                wf_occ3d[nx, ny, nz, :] = fourtransf_occ(x, y, z)
-                wf_val3d[nx, ny, nz, :] = fourtransf_val(x, y, z)
-
-    # Write WFs into file
-    with open('WannierLoc.pickle', 'wb') as f:
-        pickle.dump([wf_occ3d, wf_val3d, n_x3d], f)
-
-
-def wannier_hybrid(n_xhybr, n_hybr_uc):
-    wf_hybr = np.empty((n_xhybr, 2), dtype=complex)
-
-    for nx in range(n_xhybr):
-        x = nx * n_hybr_uc / n_xhybr
-        wf_hybr[nx, :] = fourtransf_occ_1d(x)
+def wannier_hybrid(n_xhybr, nk):
+    """Calculate Hybrid Wannier function at n_xhybr integer points"""
+    wf_hybr = fourtransf_occ_1d(nk)[0:n_xhybr, :]
 
     # Write Hybrid Wannier function into file
     with open('HybridWannierLoc.pickle', 'wb') as f:
-        pickle.dump([wf_hybr, n_xhybr, n_hybr_uc], f)
+        pickle.dump([wf_hybr, n_xhybr], f)
 
 
+# Parameters
 t = 1
 h = 0
-Nk = 1024
+# Large number of points in fft for direction with R dependence
+Nksmall = 50
+Nk = 100000
 
-kx = np.linspace(0, 2 * pi, Nk)
-ky = np.linspace(0, 2 * pi, Nk)
-kz = np.linspace(0, 2 * pi, Nk)
-
-# Cartesian coordinates, indexing for correct order x,y,z (not y,x,z)
-[kkx, kky, kkz] = np.meshgrid(kx, ky, kz, indexing='ij')
-
-Nx1d = 210
-Nunitcell = 30
+# Number of R points
+Nx1d = 16
 Nx3d = 21       # odd
-Nxhybr = 400
-Nhybr_uc = 80
+Nxhybr = 16
 
-wannier_hybrid(Nxhybr, Nhybr_uc)
+# Specify which function we want to calculate (Hybrid or normal)
+# Specify in which direction we want to check the dependance
+wannier_1d(Nx1d, Nksmall, Nksmall, Nk)
