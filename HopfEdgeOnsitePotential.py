9ad7f127e3838f452fe60d3275586433f399b542
Aleksandra <alexandra@dhcp-165-237.physik.uzh.ch>
2019-04-03 18:29:56 +0200

    Plot edge states and onsite potential on high symmetry lines

--- HopfEdgeOnsitePotential.py
+++ HopfEdgeOnsitePotential.py
@@ -6,59 +6,66 @@
 """
 
 import numpy as np
-from math import pi
+from math import pi, sqrt
 import matplotlib.pyplot as plt
 from matplotlib.colors import LinearSegmentedColormap
 
 # Parameters
-h = 2
+h = 0.5
 t = 1
 
 Nx = 101
-Ny = 101
-Nz = 16
+N_slab = 16
 
-kx = np.linspace(0, 2 * pi, Nx)
-ky = np.linspace(0, 2 * pi, Ny)
-
-[kkx, kky] = np.meshgrid(kx, ky, indexing='ij')
+Kx = np.append(np.linspace(0, pi, round(Nx * sqrt(2))), np.ones(Nx) * pi)
+Kx = np.append(Kx, np.linspace(pi, 0, Nx))
+Ky = np.append(np.linspace(0, pi, round(Nx * sqrt(2))), np.linspace(pi, 0, Nx))
+Ky = np.append(Ky, np.zeros(Nx))
+Ntotal = 2 * Nx + round(Nx * sqrt(2))
 
 # Define the shape of Hamiltonian
-H = np.zeros((Nx, Ny, 2 * Nz, 2 * Nz), dtype=complex)
+H = np.zeros((Ntotal, 2 * N_slab, 2 * N_slab), dtype=complex)
 
 # Construct blockes for Hopf Hamiltonian
-A = (np.power(np.sin(kkx), 2) + t ** 2 * np.power(np.sin(kky), 2) -
-     np.power(np.cos(kkx) + np.cos(kky) + h, 2) - 1)
-B = - np.cos(kkx) - np.cos(kky) - h
-C = 2 * np.multiply(t * np.sin(kky) + 1j * np.sin(kkx),
-                    np.cos(kkx) + np.cos(kky) + h)
-D = 2 * (t * np.sin(kky) + 1j * np.sin(kkx))
+A = (np.power(np.sin(Kx), 2) + t**2 * np.power(np.sin(Ky), 2) -
+     np.power(np.cos(Kx) + np.cos(Ky) + h, 2) - 1)
+B = - np.cos(Kx) - np.cos(Ky) - h
+C = 2 * np.multiply(t * np.sin(Ky) + 1j * np.sin(Kx),
+                    np.cos(Kx) + np.cos(Ky) + h)
+D = 2 * (t * np.sin(Ky) + 1j * np.sin(Kx))
+
+# Break the mxT symmetry:
+# kz: cos<->sin
+# B = -1j * B
+# C = 2 * np.multiply(t * np.sin(Ky + Kx) + 1j * np.sin(Kx - Ky),
+#                 np.cos(Kx - Ky) + np.cos(Ky + Kx) + h)
+# D = 2 * (t * np.sin(Ky) + 1j * np.sin(Kx))
 
 E = np.stack((np.stack((A, np.conj(C)), axis=-1),
               np.stack((C, -A), axis=-1)), axis=-1)
 
-Delta = np.stack((np.stack((B, np.zeros((Nx, Ny))), axis=-1),
+Delta = np.stack((np.stack((B, np.zeros(Ntotal)), axis=-1),
                   np.stack((D, -B), axis=-1)), axis=-1)
 
 # Surface potential
-alpha = -0.5
-V1 = np.zeros((Nx, Ny))
-V2 = alpha * np.ones((Nx, Ny))
+alpha = 1.5
+V1 = np.zeros(Ntotal)
+V2 = alpha * np.ones(Ntotal)
 
 Hsurf = np.stack((np.stack((V2, V1), axis=-1),
                   np.stack((V1, V2), axis=-1)), axis=-1)
 
 # Construct Hamiltonian for all Nz sites from these blockes
-H[:, :, 0:2, 0:2] = E
-for nz in range(0, Nz - 1):
-    H[:, :, 2 * nz + 2: 2 * nz + 4, 2 * nz + 2: 2 * nz + 4] = E
-    H[:, :, 2 * nz: 2 * nz + 2, 2 * nz + 2: 2 * nz + 4] = Delta
-    H[:, :, 2 * nz + 2: 2 * nz + 4, 2 * nz: 2 * nz + 2] = (
-        np.transpose(np.conj(Delta), (0, 1, 3, 2)))
+H[:, 0:2, 0:2] = E
+for nz in range(0, N_slab - 1):
+    H[:, 2 * nz + 2: 2 * nz + 4, 2 * nz + 2: 2 * nz + 4] = E
+    H[:, 2 * nz: 2 * nz + 2, 2 * nz + 2: 2 * nz + 4] = Delta
+    H[:, 2 * nz + 2: 2 * nz + 4, 2 * nz: 2 * nz + 2] = (
+        np.transpose(np.conj(Delta), (0, 2, 1)))
 
 # Add surface potential
-H[:, :, 0:2, 0:2] += Hsurf
-H[:, :, -2:, -2:] += Hsurf
+H[:, 0:2, 0:2] += -Hsurf
+H[:, -2:, -2:] += Hsurf
 
 # Calculate eigenvalues and eigenvectors of H-Hopf
 [Energy, States] = np.linalg.eigh(H)
@@ -66,27 +73,19 @@ H[:, :, -2:, -2:] += Hsurf
 # Set weight of eigenstates to define which are edge states
 # Weight multiplier
 lamb = 0.5
-# zline = np.arange(2 * Nz)
 
 # We take into accont that each atom has two orbitals
 # which should have the same weight
-zline = np.stack((np.arange(Nz), np.arange(Nz)), axis=-1)
-zline = np.reshape(zline, 2 * Nz, order='C')
+zline = np.stack((np.arange(N_slab), np.arange(N_slab)), axis=-1)
+zline = np.reshape(zline, 2*N_slab, order='C')
 weight = np.exp(-lamb * zline)
 # Left eigenstate
 L = np.sum(np.multiply(np.power(np.abs(States), 2),
-                       weight[np.newaxis, np.newaxis, :, np.newaxis]), axis=-2)
+           weight[np.newaxis, :, np.newaxis]), axis=-2)
 # Right eigenstate
 R = np.sum(np.multiply(np.power(np.abs(States), 2),
-                       np.flip(weight[np.newaxis, np.newaxis, :, np.newaxis],
-                               axis=-2)),
+           np.flip(weight[np.newaxis, :, np.newaxis], axis=-2)),
            axis=-2)
-
-# fig = plt.figure()
-# ax = fig.add_subplot(111, projection='3d')
-# ax.plot_surface(kkx, kky, Energy[:, :, 15])
-# ax.plot_surface(kkx, kky, Energy[:, :, 16])
-# plt.show()
 
 # Define colormap
 cdict1 = {'red': ((0.0, 0.0, 0.0),
@@ -103,17 +102,41 @@ cdict1 = {'red': ((0.0, 0.0, 0.0),
 
 blue_red1 = LinearSegmentedColormap('BlueRed1', cdict1)
 
-kxrep = np.transpose(np.tile(kx, (2 * Nz, 1)))
-kyrep = np.transpose(np.tile(ky, (2 * Nz, 1)))
-nkx = 50
-fig = plt.figure()
-# plt.scatter(kyrep, Energy[nkx, :, :], c = L[nkx, :, :] - R[nkx, :, :],
-#             s = 1, cmap=blue_red1)
-plt.scatter(kyrep, Energy[:, nkx, :], c=L[:, nkx, :] - R[:, nkx, :], s=1,
-            cmap=blue_red1)
-plt.colorbar()
-plt.show()
-# cross = (np.abs(Energy[:, :, 15]-Energy[:, :, 16])<0.05)
-# plt.figure()
-# plt.imshow(cross, cmap='winter')
+
+Kxplot = np.append(np.linspace(0, 1, round(Nx * sqrt(2))) * sqrt(2),
+                   np.linspace(0, 1, Nx) + sqrt(2))
+Kxplot = np.append(Kxplot, np.linspace(0, 1, Nx) + sqrt(2) + 1)
+Kxrep = np.transpose(np.tile(Kxplot, (2 * N_slab, 1)))
+xcoords = [0, sqrt(2), 1 + sqrt(2), 2 + sqrt(2)]
+
+# Plot the spectrum
+# Size foe 'Energy' label
+# fig = plt.figure(figsize=(1.78, 1.5))
+# ax = fig.add_axes([0.22, 0.15, 0.7303, 0.83])
+# Size without labels
+# fig = plt.figure(figsize=(1.68, 1.5))
+# ax = fig.add_axes([0.2024, 0.15, 0.7738, 0.83])
+# Size with colorbar
+fig = plt.figure(figsize=(2, 1.5))
+ax = fig.add_axes([0.17, 0.15, 0.65, 0.83])
+colax = fig.add_axes([0.85, 0.15, 0.05, 0.83])
+fs = 10
+fss = 8
+lw = 1.2
+# ax.set_ylabel('Energy', fontsize=fs)
+ax.yaxis.set_label_coords(-0.15, 0.5)
+ax.tick_params(width=lw, labelsize=fss)
+for axis in ['top', 'bottom', 'left', 'right']:
+    ax.spines[axis].set_linewidth(lw)
+Spectr = ax.scatter(Kxrep, Energy, c=L[:, :] - R[:, :], s=0.004, cmap=blue_red1)
+ax.set_xlim(0, 2 + sqrt(2))
+ax.set_xticks(xcoords)
+ax.set_xticklabels(['$\Gamma$', 'M', 'K', '$\Gamma$'], fontsize=fs)
+for xc in xcoords:
+    ax.axvline(x=xc, color='k', linewidth=lw)
+cbar = fig.colorbar(Spectr, cax=colax)
+cbar.ax.tick_params(labelsize=fss, width=lw, labelrotation=90)
+for axis in ['top', 'bottom', 'left', 'right']:
+    cbar.ax.spines[axis].set_linewidth(lw)
 # plt.show()
+plt.savefig('Surfpmpot15_h05t1.png', bbox_inches=None)
--- HopfEdgeStates.py
+++ HopfEdgeStates.py
@@ -10,48 +10,53 @@ Created on Thu Jul 19 10:48:01 2018
 # Hamiltonian depends on (kx,ky,z)
 
 import numpy as np
-from math import pi
+from math import pi, sqrt
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
+# from mpl_toolkits.mplot3d import Axes3D
 from matplotlib.colors import LinearSegmentedColormap
 
 # Parameters
-h=0.5
-t=1
+h = 0
+t = 1
 
-Nx=101
-Ny=101
-Nz=16
+Nx = 101
+Ny = 101
+N_slab = 16
 
 kx = np.linspace(0, 2*pi, Nx)
 ky = np.linspace(0, 2*pi, Ny)
+Kx = np.append(np.linspace(0, pi, round(Nx * sqrt(2))), np.ones(Nx) * pi)
+Kx = np.append(Kx, np.linspace(pi, 0, Nx))
+Ky = np.append(np.linspace(0, pi, round(Nx * sqrt(2))), np.linspace(pi, 0, Nx))
+Ky = np.append(Ky, np.zeros(Nx))
+Ntotal = 2 * Nx + round(Nx * sqrt(2))
 
-[kkx, kky] = np.meshgrid(kx, ky, indexing = 'ij')
+[kkx, kky] = np.meshgrid(kx, ky, indexing='ij')
 
 # Define the shape of Hamiltonian
-H = np.zeros((Nx, Ny, 2 * Nz, 2 * Nz), dtype = complex)
+H = np.zeros((Ntotal, 2 * N_slab, 2 * N_slab), dtype=complex)
 
 # Construct blockes for Hopf Hamiltonian
-A = (np.power(np.sin(kkx), 2) + t**2 * np.power(np.sin(kky), 2) - 
-     np.power(np.cos(kkx) + np.cos(kky) + h, 2) - 1)
-B = - np.cos(kkx) - np.cos(kky) - h
-C = 2 * np.multiply(t * np.sin(kky) + 1j * np.sin(kkx), 
-                np.cos(kkx) + np.cos(kky) + h)
-D = 2 * (t * np.sin(kky) + 1j * np.sin(kkx))
+A = (np.power(np.sin(Kx), 2) + t**2 * np.power(np.sin(Ky), 2) - 
+     np.power(np.cos(Kx) + np.cos(Ky) + h, 2) - 1)
+B = - np.cos(Kx) - np.cos(Ky) - h
+C = 2 * np.multiply(t * np.sin(Ky) + 1j * np.sin(Kx), 
+                    np.cos(Kx) + np.cos(Ky) + h)
+D = 2 * (t * np.sin(Ky) + 1j * np.sin(Kx))
 
-E = np.stack((np.stack((A, np.conj(C)), axis = -1), 
-              np.stack((C, -A), axis = -1)), axis = -1)
+E = np.stack((np.stack((A, np.conj(C)), axis=-1),
+              np.stack((C, -A), axis=-1)), axis=-1)
 
-Delta = np.stack((np.stack((B, np.zeros((Nx, Ny))), axis = -1), 
-              np.stack((D, -B), axis = -1)), axis = -1)
+Delta = np.stack((np.stack((B, np.zeros(Ntotal)), axis=-1),
+                  np.stack((D, -B), axis=-1)), axis=-1)
 
-# Construct Hamiltonian for all Nz sites from these blockes
-H[:, :, 0:2, 0:2] = E
-for nz in range(0, Nz-1):
-    H[:, :, 2*nz + 2 : 2*nz + 4, 2*nz + 2 : 2*nz + 4] = E
-    H[:, :, 2*nz : 2*nz + 2, 2*nz + 2 : 2*nz + 4] = Delta
-    H[:, :, 2*nz + 2 : 2*nz + 4, 2*nz : 2*nz + 2] = (
-            np.transpose(np.conj(Delta), (0, 1, 3, 2)))
+# Construct Hamiltonian for all N_slab sites from these blockes
+H[:, 0:2, 0:2] = E
+for nz in range(0, N_slab-1):
+    H[:, 2*nz + 2: 2*nz + 4, 2*nz + 2: 2*nz + 4] = E
+    H[:, 2*nz: 2*nz + 2, 2*nz + 2: 2*nz + 4] = Delta
+    H[:, 2*nz + 2: 2*nz + 4, 2*nz: 2*nz + 2] = (
+            np.transpose(np.conj(Delta), (0, 2, 1)))
    
 # Calculate eigenvalues and eigenvectors of H-Hopf
 [Energy, States] = np.linalg.eigh(H)
@@ -59,52 +64,56 @@ for nz in range(0, Nz-1):
 # Set weight of eigenstates to define which are edge states
 # Weight multiplier
 lamb = 0.5
-#zline = np.arange(2 * Nz)
+# zline = np.arange(2 * N_slab)
 
 # We take into accont that each atom has two orbitals 
 # which should have the same weight
-zline=np.stack((np.arange(Nz),np.arange(Nz)),axis=-1)
-zline=np.reshape(zline, 2*Nz, order='C')
+zline = np.stack((np.arange(N_slab), np.arange(N_slab)), axis=-1)
+zline = np.reshape(zline, 2*N_slab, order='C')
 weight = np.exp(-lamb * zline)
 # Left eigenstate
-L = np.sum(np.multiply(np.power(np.abs(States),2), 
-    weight[np.newaxis, np.newaxis, :, np.newaxis]), axis = -2)
+L = np.sum(np.multiply(np.power(np.abs(States), 2),
+           weight[np.newaxis, :, np.newaxis]), axis=-2)
 # Right eigenstate
-R = np.sum(np.multiply(np.power(np.abs(States),2), 
-    np.flip(weight[np.newaxis, np.newaxis, :, np.newaxis], axis=-2)), 
-    axis = -2)
+R = np.sum(np.multiply(np.power(np.abs(States), 2),
+           np.flip(weight[np.newaxis, :, np.newaxis], axis=-2)),
+           axis=-2)
 
 
-#fig = plt.figure()
-#ax = fig.add_subplot(111, projection='3d')
-#ax.plot_surface(kkx, kky, Energy[:, :, 15])
-#ax.plot_surface(kkx, kky, Energy[:, :, 16])
-#plt.show()
+# fig = plt.figure()
+# ax = fig.add_subplot(111, projection='3d')
+# ax.plot_surface(kkx, kky, Energy[:, :, 15])
+# ax.plot_surface(kkx, kky, Energy[:, :, 16])
+# plt.show()
 
 # Define colormap 
 cdict1 = {'red':   ((0.0, 0.0, 0.0),
-                   (0.5, 0.0, 0.1),
-                   (1.0, 1.0, 1.0)),
+                    (0.5, 0.0, 0.1),
+                    (1.0, 1.0, 1.0)),
 
-         'green': ((0.0, 0.0, 0.0),
-                   (1.0, 0.0, 0.0)),
+          'green': ((0.0, 0.0, 0.0),
+                    (1.0, 0.0, 0.0)),
 
-         'blue':  ((0.0, 0.0, 1.0),
-                   (0.5, 0.1, 0.0),
-                   (1.0, 0.0, 0.0))
-        }
+          'blue':  ((0.0, 0.0, 1.0),
+                    (0.5, 0.1, 0.0),
+                    (1.0, 0.0, 0.0))
+          }
 
 blue_red1 = LinearSegmentedColormap('BlueRed1', cdict1)
 
-kxrep = np.transpose(np.tile(kx, (2 * Nz, 1)))
-kyrep = np.transpose(np.tile(ky, (2 * Nz, 1)))
-nkx = 50
-fig = plt.figure()
-#plt.scatter(kyrep, Energy[nkx, :, :], c = L[nkx, :, :] - R[nkx, :, :], s = 1, cmap=blue_red1)
-plt.scatter(kyrep, Energy[:, nkx, :], c = L[:, nkx, :] - R[:, nkx, :], s = 1, cmap=blue_red1)
-plt.colorbar()
 
-#cross = (np.abs(Energy[:, :, 15]-Energy[:, :, 16])<0.05)
-#plt.figure()
-#plt.imshow(cross, cmap='winter')
-#plt.show()
+Kxplot = np.append(np.linspace(0, 1, round(Nx * sqrt(2))) * sqrt(2),
+                   np.linspace(0, 1, Nx) + sqrt(2))
+Kxplot = np.append(Kxplot, np.linspace(0, 1, Nx) + sqrt(2) + 1)
+Kxrep = np.transpose(np.tile(Kxplot, (2 * N_slab, 1)))
+fig = plt.figure()
+# plt.scatter(kyrep, Energy[nkx, :, :], c = L[nkx, :, :] - R[nkx, :, :],
+# s = 1, cmap=blue_red1)
+plt.scatter(Kxrep, Energy, c=L[:, :] - R[:, :], s=1, cmap=blue_red1)
+plt.colorbar()
+plt.show()
+
+# cross = (np.abs(Energy[:, :, 15]-Energy[:, :, 16])<0.05)
+# plt.figure()
+# plt.imshow(cross, cmap='winter')
+# plt.show()
