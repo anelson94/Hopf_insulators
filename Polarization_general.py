f14f6809b39f79f051bf528417e87046a8da4c61
alexandra <alexsandranelsn94@gmail.com>
2020-02-03 18:26:41 +0100

    Add symmetry breaking function
    Caclculate polarization of a Hamiltonian with broken symmetries.
    Add function that checks new module hopfham.py
    Create series of plots for polarization at different parameters.

--- Polarization_general.py
+++ Polarization_general.py
@@ -10,6 +10,7 @@ from math import pi
 import hopfham
 from HopfChernNumbers import gap_check
 import matplotlib.pyplot as plt
+import pickle
 
 
 def projector(u):
@@ -21,7 +22,7 @@ def projector(u):
     return proj
 
 
-def polarization_z(pr):
+def polarization_z(pr, between01):
     """Polarization of a 1D cut of a BZ in kz direction"""
     nz = pr.shape[2]
     all_pr = np.identity(2, dtype=complex)
@@ -29,39 +30,148 @@ def polarization_z(pr):
     for idz in range(nz):
         all_pr = np.matmul(all_pr, pr[:, :, idz, :, :])
     pol = np.angle(np.trace(all_pr, axis1=-2, axis2=-1)) / 2 / pi
-    pol = np.where(pol > 0, pol, pol + 1)
+    if between01 == 1:
+        pol = np.where(pol > 0, pol, pol + 1)
 
     return pol
 
 
+def polarization_x(pr, between01):
+    """Polarization of a 1D cut of a BZ in kx direction"""
+    nx = pr.shape[0]
+    all_pr = np.identity(2, dtype=complex)
+    all_pr = all_pr[np.newaxis, np.newaxis, :, :]
+    for idx in range(nx):
+        all_pr = np.matmul(all_pr, pr[idx, :, :, :, :])
+    pol = np.angle(np.trace(all_pr, axis1=-2, axis2=-1)) / 2 / pi
+    if between01 == 1:
+        pol = np.where(pol > 0, pol, pol + 1)
+
+    return pol
+
+
+def sym_break_draft(ampl, kx, ky, kz):
+    """Try different symmetry breaking terms in Hamiltonian"""
+    nx, ny, nz = kx.shape
+
+    sigmax = np.array([[0, 1], [1, 0]])
+    sigmay = np.array([[0, -1j], [1j, 0]])
+    sigmaz = np.array([[1, 0], [0, -1]])
+
+    # Pauli matrices for calculations at all (kx, ky, kz)
+    sigmax = sigmax[np.newaxis, np.newaxis, np.newaxis, :, :]
+    sigmay = sigmay[np.newaxis, np.newaxis, np.newaxis, :, :]
+    sigmaz = sigmaz[np.newaxis, np.newaxis, np.newaxis, :, :]
+
+    zero_k = np.zeros((nx, ny, nz))
+
+    hx = ampl * (np.sin(kz) + np.cos(kz)) / 2  # 0.5 * np.cos(kx) +
+    hy = ampl * np.cos(kz)  # 0.3 * np.cos(ky) * np.cos(kx) +
+    hz = zero_k  # 0.2 * np.cos(kx) + 0.3 * np.sin(ky) + 0.4 * np.sin(kz)
+
+    hx = hx[:, :, :, np.newaxis, np.newaxis]
+    hy = hy[:, :, :, np.newaxis, np.newaxis]
+    hz = hz[:, :, :, np.newaxis, np.newaxis]
+
+    return hx * sigmax + hy * sigmay + hz * sigmaz
+
+
+def hamiltonian_checkes(hamilt):
+    """Check that old and new codes give the same results"""
+    with open('HopfHamiltonian.pickle', 'rb') as f:
+        hamilt_old = pickle.load(f)
+
+    print('Two hamiltonians are equal:', np.allclose(hamilt, hamilt_old))
+    print('Maximal difference between two Hamiltonians:',
+          np.max(np.abs(hamilt - hamilt_old)))
+
+    [e, u] = np.linalg.eigh(hamilt)
+
+    [e2, u2] = np.linalg.eigh(hamilt)
+
+    print('Two eigh calculations are equal:', np.allclose(u, u2))
+
+    # with open('Hopfeigen.pickle', 'rb') as f:
+    #     [E_old, u_old] = pickle.load(f)
+    [E_old, u_old] = np.linalg.eigh(hamilt_old)
+    uocc_old = u_old[:, :, :, :, 0]
+
+    uocc = u[:, :, :, :, 0]
+    uocc_smooth = hopfham.smooth_gauge(uocc)
+
+    with open('Hopfsmoothstates.pickle', 'rb') as f:
+        usmooth_old = pickle.load(f)
+
+    print(np.allclose(uocc_smooth, usmooth_old))
+    print(np.allclose(uocc, uocc_old))
+
+    phase_diff = np.angle(uocc_smooth[0, 0, 0, 0]) - np.angle(usmooth_old[0, 0, 0, 0])
+    usmooth_old = usmooth_old * np.exp(1j * phase_diff)
+    print(phase_diff)
+    print(u.shape, u_old.shape)
+    print(np.allclose(uocc_smooth, usmooth_old))
+
+
 def main():
     m = 5
-    nx = 100
-    ny = 100
-    nz = 100
-    plotpol = 0
+    nx = 101
+    ny = 101
+    nz = 101
+    plotpol = 2
+    between01 = 0
+    hamiltceck = 0
 
     kx = np.linspace(0, 2 * pi, nx)
     ky = np.linspace(0, 2 * pi, ny)
     kz = np.linspace(0, 2 * pi, nz)
 
     kkx, kky, kkz = np.meshgrid(kx, ky, kz, indexing='ij')
-    hamilt = hopfham.ham_mrw(m, kkx, kky, kkz)
+    # TODO calculate different amplitudes and see how the minimum of P_z changes
+    n_ampl = 1
+    ampl_start = 0.5
+    ampl_delta = 0.1
+    pol_z = np.empty((nx, ny, n_ampl), dtype=complex)
+    for idx in range(n_ampl):
+        ampl = ampl_start + idx * ampl_delta
+        hamilt = (hopfham.ham_mrw(m, kkx, kky, kkz)
+                  + sym_break_draft(ampl, kkx, kky, kkz))
 
-    [e, u] = np.linalg.eigh(hamilt)
+        if hamiltceck == 1:
+            hamiltonian_checkes(hamilt)
 
-    # Occupied states correspond to smaller eigenvalues
-    uocc = u[:, :, :, :, 0]
+        [e, u] = np.linalg.eigh(hamilt)
 
-    gap_check(e[:, :, :, 0], 0.001)
+        # Occupied states correspond to smaller eigenvalues
+        uocc = u[:, :, :, :, 0]
+        uocc_smooth = hopfham.smooth_gauge(uocc)
+        print(hopfham.hopf_invariant(uocc_smooth))
 
-    pr = projector(uocc)
+        gap_check(e[:, :, :, 0], 0.01)
 
-    pol_z = polarization_z(pr)
-    print(np.max(pol_z) - np.min(pol_z))
+        pr = projector(uocc)
+
+        pol_z[:, :, idx] = polarization_z(pr, between01)
+    if plotpol == 2:
+        pol_x = polarization_x(pr, between01)
+        print(np.max(pol_x), np.min(pol_x))
+    # print(np.max(pol_z) - np.min(pol_z))
     if plotpol == 1:
-        plt.imshow(pol_z)
-        plt.colorbar()
+        plt.plot(kx, pol_z[:, 50, :])
+        plt.plot(kx, np.zeros((nx, 1)), c='black')
+        plt.legend([round(ampl_start + idx * ampl_delta, 1)
+                    for idx in range(n_ampl)], loc='upper right')
+        plt.show()
+    if plotpol == 2:
+        fig, ax = plt.subplots(1, 2)
+        ax[0].plot(kx, pol_z[:, 50])
+        ax[0].plot(kx, np.zeros((nx, 1)), c='black')
+        # ax[1].plot(kz, pol_x[:, 50])
+        # ax[1].plot(kz, np.zeros((nx, 1)), c='black')
+        # plt.plot(kx, np.ones((nx, 1)), c='black')
+        imsh = ax[1].imshow(pol_x)
+        # plt.colorbar(imsh, cax=ax[1])
+        # plt.imshow(pol_z)
+        # plt.colorbar()
         plt.show()
 
 
