4d90ea3f23d8c241a672fdf4f95c334a4347a5f6
alexandra <alexsandranelsn94@gmail.com>
2020-02-10 13:42:38 +0100

    Change preimage creating file. Add function that generates colors. Add function that plots preimages of a list of points.

--- ImageOfS2inT3.py
+++ ImageOfS2inT3.py
@@ -14,18 +14,47 @@ from mayavi import mlab
 import hopfham
 
 
-def image(n, model, eps1=0.1, eps2=0.1):
+def color_list():
+    # Define a list with colors for preimage plotting
+    red = (1, 153 / 255, 153 / 255)
+    blue = (153 / 255, 153 / 255, 1)
+    green = (153 / 255, 1, 153 / 255)
+    brown = (174 / 255, 129 / 255, 58 / 255)
+    lightblue = (58 / 255, 138 / 255, 200 / 255)
+    return (red, green, blue, brown, lightblue)
+
+
+def preimage(n, model, eps1=0.1, eps2=0.1):
     """Preimage at T^3 of a point n at S^2 for a map model"""
-    hx = model['hx']
-    hy = model['hy']
-    hz = model['hz']
-    nx = n[0]
 
-    image = (np.isclose(model['hx'], n[0], atol=eps1, rtol=eps2)
-             & np.isclose(model['hy'], n[1], atol=eps1, rtol=eps2)
-             & np.isclose(model['hz'], n[2], atol=eps1, rtol=eps2)) * 1
+    im = (np.isclose(model['hx'], n[0], atol=eps1, rtol=eps2)
+          & np.isclose(model['hy'], n[1], atol=eps1, rtol=eps2)
+          & np.isclose(model['hz'], n[2], atol=eps1, rtol=eps2)) * 1
 
-    return image
+    return im
+
+
+def preimage_plot(model, *n_list, nx=101, ny=101, nz=101, **kwargs):
+    """Plot preimages of n_list points"""
+    kx, ky, kz = hopfham.mesh_make(nx, ny, nz)
+    if callable(model):
+        model = model(kx, ky, kz)
+
+    mlab.figure(bgcolor=(1, 1, 1))
+    i_color = 0
+    colors = color_list()
+    for n in n_list:
+        im = preimage(n, model, **kwargs)
+        con = mlab.contour3d(im, contours=2,
+                             color=colors[i_color % len(colors)],
+                             transparent=False)
+        i_color += 1
+    mlab.axes(con, xlabel='', ylabel='', zlabel='', color=(0, 0, 0))
+    mlab.show()
+
+
+def image(k, model):
+    """Find """
 
 
 def main():
@@ -33,33 +62,13 @@ def main():
     n1 = np.array([1, 0, 0])
     n2 = np.array([0, 1, 0])
     n3 = np.array([0, 0, 1])
+    # n4 = np.array([-1, 0, 0])
+    # n5 = np.array([0, -1, 0])
 
-    nx = 101
-    ny = 101
-    nz = 101
+    preimage_plot(hopfham.model_mrw_norm, n1, n2, n3)
 
-    kx, ky, kz = hopfham.mesh_make(nx, ny, nz)
-
-    model = hopfham.model_mrw_norm(kx, ky, kz)
-
-    image1 = image(n1, model)
-    image2 = image(n2, model)
-    image3 = image(n3, model)
-
-    # Plot the resulting preimage
-    mlab.figure(bgcolor=(1, 1, 1))
-    con1 = mlab.contour3d(image1, contours=2, color=(1, 153 / 255, 153 / 255),
-                          transparent=False)
-    mlab.axes(con1, xlabel='', ylabel='', zlabel='', color=(0, 0, 0))
-    mlab.contour3d(image2, contours=2, color=(153 / 255, 1, 153 / 255),
-                   transparent=False)
-    mlab.contour3d(image3, contours=2, color=(153 / 255, 153 / 255, 1),
-                   transparent=False)
-    mlab.show()
-
-    return model['hx'], model['hy'], model['hz']
 
 
 if __name__ == '__main__':
-    hx, hy, hz = main()
+    main()
 
