7873ec62c5e9cfefe4e9f2dd63b64d3a5df3e682
alexandra <alexsandranelsn94@gmail.com>
2020-02-10 15:32:53 +0100

    Change preimage file. Add plotting of the image of some points in the BZ. Plot all Hamiltonian values using scatter.

--- ImageOfS2inT3.py
+++ ImageOfS2inT3.py
@@ -12,6 +12,9 @@ They will be the linked circles
 import numpy as np
 from mayavi import mlab
 import hopfham
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from math import pi
 
 
 def color_list():
@@ -49,24 +52,52 @@ def preimage_plot(model, *n_list, nx=101, ny=101, nz=101, **kwargs):
                              color=colors[i_color % len(colors)],
                              transparent=False)
         i_color += 1
-    mlab.axes(con, xlabel='', ylabel='', zlabel='', color=(0, 0, 0))
+    mlab.axes(con, xlabel='x', ylabel='y', zlabel='z', color=(0, 0, 0))
     mlab.show()
 
 
-def image(k, model):
+def image_plot(kx, ky, kz, model):
     """Find """
+    if callable(model):
+        model = model(kx, ky, kz)
+
+    # Plot images of (kx, ky, kz)
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    ax.scatter(model['hx'], model['hy'], model['hz'])
+
+    plt.show()
+
+    return model['hx'], model['hy'], model['hz']
 
 
 def main():
-    # Three points in S2 (3d vectors with unitary norms)
-    n1 = np.array([1, 0, 0])
-    n2 = np.array([0, 1, 0])
-    n3 = np.array([0, 0, 1])
-    # n4 = np.array([-1, 0, 0])
-    # n5 = np.array([0, -1, 0])
+    preimageplot = 1
+    imageplot = 0
 
-    preimage_plot(hopfham.model_mrw_norm, n1, n2, n3)
+    if preimageplot == 1:
+        # Three points in S2 (3d vectors with unitary norms)
+        n = np.array([-0.3696628, 0, -0.92916596])
+        # n = np.array([-0.459850, 0.136869, -0.8773850])
+        n1 = np.array([1, 0, 0])
+        # n2 = np.array([0, 1, 0])
+        # n3 = np.array([0, 0, 1])
+        # n4 = np.array([-1, 0, 0])
+        # n5 = np.array([0, -1, 0])
 
+        preimage_plot(hopfham.model_mrw_norm, n, n1, nx=401, ny=401, nz=401,
+                      eps1=0.05, eps2=0.05)
+
+    if imageplot == 1:
+        # kx = np.ones(100) * pi
+        # ky = np.ones(100) * 2.1
+        # kz = np.linspace(-pi, pi, 100)
+        kx = pi
+        ky = 2.1
+        kz = -1.3
+        hx, hy, hz = image_plot(kx, ky, kz, hopfham.model_mrw_norm)
+        print(hx, hy, hz)
 
 
 if __name__ == '__main__':
