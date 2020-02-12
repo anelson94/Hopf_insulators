9add4aa56aa25c03b2f3eef3979243ca55f35f62
alexandra <alexsandranelsn94@gmail.com>
2020-02-12 18:37:34 +0100

    Add comments

--- hopfham.py
+++ hopfham.py
@@ -43,7 +43,7 @@ def ham(kx, ky, kz, model, **kwargs):
 
 def model_mrw(kx, ky, kz, m=1):
     """Moore Ran Wen model of Hamiltonian"""
-
+    # Define coefficients in front of Pauli matrices
     hx = 2 * (np.sin(kx) * np.sin(kz)
               + np.sin(ky) * (np.cos(kx) + np.cos(ky) + np.cos(kz) - 3 + m))
     hy = 2 * (- np.sin(ky) * np.sin(kz)
@@ -51,13 +51,13 @@ def model_mrw(kx, ky, kz, m=1):
     hz = (np.sin(kx) ** 2 + np.sin(ky) ** 2
           - np.sin(kz) ** 2 - (
                       np.cos(kx) + np.cos(ky) + np.cos(kz) - 3 + m) ** 2)
-
+    # Return them as a dict
     return {'hx': hx, 'hy': hy, 'hz': hz}
 
 
 def model_mrw_norm(kx, ky, kz, m=1):
     """Moore Ran Wen model of Hamiltonian"""
-
+    # Define coefficients in front of Pauli matrices
     hx = 2 * (np.sin(kx) * np.sin(kz)
               + np.sin(ky) * (np.cos(kx) + np.cos(ky) + np.cos(kz) - 3 + m))
     hy = 2 * (- np.sin(ky) * np.sin(kz)
@@ -66,10 +66,11 @@ def model_mrw_norm(kx, ky, kz, m=1):
           - np.sin(kz) ** 2 - (
                       np.cos(kx) + np.cos(ky) + np.cos(kz) - 3 + m) ** 2)
 
+    # Define normalization
     lamb = (np.sin(kx) ** 2 + np.sin(ky) ** 2
             + np.sin(kz) ** 2 + (
                       np.cos(kx) + np.cos(ky) + np.cos(kz) - 3 + m) ** 2)
-
+    # Return normalized coefficients as a dict
     return {'hx': hx / lamb, 'hy': hy / lamb, 'hz': hz / lamb}
 
 
@@ -80,45 +81,58 @@ def model_edgeconst(kx, ky, kz):
     sigmay = np.array([[0, -1j], [1j, 0]])
     sigmaz = np.array([[1, 0], [0, -1]])
 
+    # Define psi, theta and phi angles to transform cube to a sphere
+    # This allows to get constant values on the edges of the cube.
+    # TODO understand how
     k_norm = np.sqrt(kx ** 2 + ky ** 2 + kz ** 2)
     psi = np.maximum(np.abs(kx),
                      np.maximum(np.abs(ky), np.abs(kz)))
 
     # remove 0 elements from norm for further division
     k_norm_fixed = np.where(k_norm > 0, k_norm, 1)  # delta_k / 2
+    # Where k_norm = 0 define theta = pi/2
     theta = np.where(k_norm > 0, np.arccos(kz / k_norm_fixed), pi / 2)
 
     # remove 0 elements from kx for further division
     kx_fixed = np.where(np.abs(kx) > 0, kx, 1)
+    # Where kx = 0 define phi = pi/2 with a pi shift for negative ky
     phi = np.where(
         np.abs(kx) > 0,
         np.arctan(ky / kx_fixed) + pi * np.heaviside(-kx, 0),
         pi / 2 + pi * np.heaviside(-ky, 0)
     )
 
+    # Define 4 components of a vector on S3 (Re and Im parts of c1, c2)
     c1 = np.cos(psi) + 1j * np.sin(psi) * np.cos(theta)
     c2 = np.sin(psi) * np.sin(theta) * (np.cos(phi) + 1j * np.sin(phi))
 
+    # Define coefficients in front of Pauli matrices
     c = np.stack((c1, c2), axis=-1)
 
     hx = np.sum(np.matmul(np.conj(c), sigmax) * c, axis=-1)
     hy = np.sum(np.matmul(np.conj(c), sigmay) * c, axis=-1)
     hz = np.sum(np.matmul(np.conj(c), sigmaz) * c, axis=-1)
 
+    # Return them as a dict
     return {'hx': hx, 'hy': hy, 'hz': hz}
 
 
 def model_mrw_maps(kx, ky, kz, m=1):
     """Moore Ran Wen model constructed from maps T3->S3->S2"""
+    # Define a vector on S3
     c1, c2, c3, c4 = map_t3s3_mrw(kx, ky, kz, m)
+    # Define a vector on S2
     hx, hy, hz = map_s3s2(c1, c2, c3, c4)
     return {'hx': hx, 'hy': hy, 'hz': hz}
 
 
 def model_mrw_maps_rotated(kx, ky, kz, m=1, alpha=pi/2):
     """Rotated Moore Ran Wen model constructed from maps T3->S3->S2"""
+    # Define a vector on S3
     c1, c2, c3, c4 = map_t3s3_mrw(kx, ky, kz, m)
+    # Rotate a vector on S3
     c1, c2, c3, c4 = s3_rotate(c1, c2, c3, c4, alpha)
+    # Define a vector on S2
     hx, hy, hz = map_s3s2(c1, c2, c3, c4)
     return {'hx': hx, 'hy': hy, 'hz': hz}
 
@@ -126,7 +140,9 @@ def model_mrw_maps_rotated(kx, ky, kz, m=1, alpha=pi/2):
 def model_edgeconst_maps(kx, ky, kz):
     """Model that is constant on the edge of the BZ
     from maps T3->S3->S2"""
+    # Define a vector on S3
     c1, c2, c3, c4 = map_t3s3_edgeconst(kx, ky, kz)
+    # Define a vector on S2
     hx, hy, hz = map_s3s2(c1, c2, c3, c4)
     return {'hx': hx, 'hy': hy, 'hz': hz}
 
@@ -134,8 +150,11 @@ def model_edgeconst_maps(kx, ky, kz):
 def model_edgeconst_maps_rotated(kx, ky, kz, alpha):
     """Rotated model that is constant on the edge of the BZ
     from maps T3->S3->S2"""
+    # Define a vector on S3
     c1, c2, c3, c4 = map_t3s3_edgeconst(kx, ky, kz)
+    # Rotate a vector on S3
     c1, c2, c3, c4 = s3_rotate(c1, c2, c3, c4, alpha)
+    # Define a vector on S2
     hx, hy, hz = map_s3s2(c1, c2, c3, c4)
     return {'hx': hx, 'hy': hy, 'hz': hz}
 
@@ -151,21 +170,26 @@ def map_t3s3_mrw(kx, ky, kz, m):
 
 def map_t3s3_edgeconst(kx, ky, kz):
     """Map from T3 to S3 that is constant on the edge of the BZ"""
+    # Define psi, theta and phi angles to transform cube to a sphere
+    # This allows to get constant values on the edges of the cube.
     k_norm = np.sqrt(kx ** 2 + ky ** 2 + kz ** 2)
     psi = np.maximum(np.abs(kx),
                      np.maximum(np.abs(ky), np.abs(kz)))
 
     # remove 0 elements from norm for further division
-    k_norm_fixed = np.where(k_norm > 0, k_norm, 1)  # delta_k / 2
+    k_norm_fixed = np.where(k_norm > 0, k_norm, 1)
+    # Where k_norm = 0 define theta = pi/2
     theta = np.where(k_norm > 0, np.arccos(kz / k_norm_fixed), pi / 2)
 
     # remove 0 elements from kx for further division
     kx_fixed = np.where(np.abs(kx) > 0, kx, 1)
+    # Where kx = 0 define phi = pi/2 with a pi shift for negative ky
     phi = np.where(
         np.abs(kx) > 0,
         np.arctan(ky / kx_fixed) + pi * np.heaviside(-kx, 0),
         pi / 2 + pi * np.heaviside(-ky, 0)
     )
+    # Define 4 components of a vector on S3
     c1 = np.cos(psi)
     c2 = np.sin(psi) * np.cos(theta)
     c3 = np.sin(psi) * np.sin(theta) * np.cos(phi)
@@ -185,6 +209,7 @@ def s3_rotate(c1, c2, c3, c4, alpha):
 
 def map_s3s2(c1, c2, c3, c4):
     """Normalized Hopf map from S3 to S2"""
+    # Define complex 2-vector from a 4-vector on S3
     z1 = c1 + 1j * c2
     z2 = c3 + 1j * c4
 
@@ -193,14 +218,18 @@ def map_s3s2(c1, c2, c3, c4):
     sigmay = np.array([[0, -1j], [1j, 0]])
     sigmaz = np.array([[1, 0], [0, -1]])
 
+    # Calculate coefficients of a vector on S2 using formula
+    # h_i=c^+ sigma_i c
     z = np.stack((z1, z2), axis=-1)
 
     hx = np.real(np.sum(np.matmul(np.conj(z), sigmax) * z, axis=-1))
     hy = np.real(np.sum(np.matmul(np.conj(z), sigmay) * z, axis=-1))
     hz = np.real(np.sum(np.matmul(np.conj(z), sigmaz) * z, axis=-1))
 
+    # Normalization coefficient
     lamb = np.sqrt(hx**2 + hy**2 + hz**2)
 
+    # Return normalized components of a vector on S2
     return hx / lamb, hy / lamb, hz / lamb
 
 
@@ -232,6 +261,7 @@ def ham_mrw(m, kx, ky, kz):
     hy = hy[:, :, :, np.newaxis, np.newaxis]
     hz = hz[:, :, :, np.newaxis, np.newaxis]
 
+    # Return a Hamiltonian 2*2 matrix for a k-grid
     return hx * sigmax + hy * sigmay + hz * sigmaz
 
 
@@ -268,6 +298,7 @@ def ham_bzedge_constant(kx, ky, kz):
     hy = np.sum(np.matmul(np.conj(c), sigmay) * c, axis=-1)
     hz = np.sum(np.matmul(np.conj(c), sigmaz) * c, axis=-1)
 
+    # Return a Hamiltonian 2*2 matrix for a k-grid
     return (hx[..., np.newaxis, np.newaxis] * sigmax
             + hy[..., np.newaxis, np.newaxis] * sigmay
             + hz[..., np.newaxis, np.newaxis] * sigmaz)
@@ -275,7 +306,10 @@ def ham_bzedge_constant(kx, ky, kz):
 
 def parallel_transport_1d(u, n):
     """Perform parallel transport of vector len=n in 1 direction for a
-    stack of vectors"""
+    stack of vectors
+
+    Each next element is multiplied by a phase overlap with previous element
+    with reverted sign. A smooth phase is obtained in the chosen direction."""
     for nk in range(0, n - 1):
         m_old = scalarprod(u[..., nk, :], u[..., nk + 1, :])
         u[..., nk + 1, :] = (u[..., nk + 1, :]
@@ -295,8 +329,11 @@ def smooth_gauge(u):
     # The function gains the multiplier
     lamb = scalarprod(u[0, 0, 0, :], u[nx - 1, 0, 0, :])
 
+    # A vector that helps to distribute the multiplier along BZ
     nxs = np.linspace(0, nx - 1, nx) / (nx - 1)
     # Distribute the multiplier among functions at kx in [0, 2pi]
+    # Each point gets portion different by lamb / (nx - 1) from the
+    # previous point
     u[:, 0, 0, :] = u[:, 0, 0, :] * np.exp(
         - 1j * np.angle(lamb) * nxs[:, np.newaxis])
 
@@ -310,12 +347,14 @@ def smooth_gauge(u):
     langle2 = np.angle(lamb2)
 
     # Construct smooth phase of lambda (without 2pi jumps)
+    # Add 2pi shift at each jump
     for nkx in range(0, nx - 1):
         if np.abs(langle2[nkx + 1] - langle2[nkx]) > pi:
             langle2[nkx + 1: nx] = (
                     langle2[nkx + 1: nx]
                     - np.sign(langle2[nkx + 1] - langle2[nkx]) * (2 * pi))
 
+    # A vector that helps to distribute the multiplier along BZ
     nys = np.linspace(0, ny - 1, ny) / (ny - 1)
     # Distribute the multiplier among functions at ky in [0, 2pi]
     u[:, :, 0, :] = (u[:, :, 0, :] * np.exp(
@@ -325,12 +364,14 @@ def smooth_gauge(u):
     # For all kx, ky make parallel transport along kz
     u = parallel_transport_1d(u, nz)
 
+    # The function gains a multiplier
     lamb3 = scalarprod(u[:, :, 0, :], u[:, :, nz - 1, :])
+    # Define a phase
     langle3 = np.angle(lamb3)
 
     # Langle3 = np.where(Langle3 < 0, Langle3 + 2 * pi, Langle3)
 
-    # First make the lambda phase smooth along x-axis
+    # First make the lambda phase smooth (remove 2pi jumps) along x-axis
     for nkx in range(0, nx - 1):
         jump = (np.abs(langle3[nkx + 1, :] - langle3[nkx, :])
                 > pi * np.ones(ny))
@@ -339,13 +380,15 @@ def smooth_gauge(u):
         langle3[nkx + 1: nx, :] = (langle3[nkx + 1: nx, :] -
                                    langlechange[np.newaxis, :])
 
-    # Then make the phase smooth along y-axis similar for all x
+    # Then make the phase smooth (remove 2pi jumps) along y-axis similar
+    # for all x
     for nky in range(0, ny - 1):
         if np.abs(langle3[0, nky + 1] - langle3[0, nky]) > pi:
             langle3[:, nky + 1: ny] = (
                     langle3[:, nky + 1: ny]
                     - np.sign(langle3[0, nky + 1] - langle3[0, nky]) * (2 * pi))
 
+    # A vector that helps to distribute the multiplier along BZ
     nzs = np.linspace(0, nz - 1, nz) / (nz - 1)
     # Distribute the multiplier among functions at kz in [0, 2pi]
     u = (u * np.exp(- 1j * langle3[:, :, np.newaxis, np.newaxis]
@@ -356,7 +399,7 @@ def smooth_gauge(u):
 
 def hopf_invariant(u):
     """Calculate Hopf invariant for the band u"""
-    # Constract the overlaps between neighbor points in all possible directions
+    # Construct the overlaps between neighbor points in all possible directions
     uxy1 = scalarprod(u[:-1, :-1, :-1, :], u[1:, :-1, :-1, :])
     uxy2 = scalarprod(u[1:, :-1, :-1, :], u[1:, 1:, :-1, :])
     uxy3 = scalarprod(u[1:, 1:, :-1, :], u[:-1, 1:, :-1, :])
@@ -376,16 +419,19 @@ def hopf_invariant(u):
             + np.angle(uyz1 * uyz2 * uyz3 * np.conj(uzx1)) * np.angle(uxy1)
             + np.angle(uzx1 * uzx2 * uzx3 * np.conj(uxy1)) * np.angle(uyz1))
 
-    # Hopf invariant is a sum of A*F over the whole BZ
+    # Hopf invariant is a sum of A*F over the whole BZ. '-' sign is a
+    # matter of definition
     return - np.sum(underhopf)/(2*pi)**2
 
 
 def mesh_make(nx, ny, nz):
     """Make a 3D mesh for coordinate vectors with (nx, ny, nz) points"""
+    # Define k vector components in the interval (-pi, pi)
     kx = np.linspace(-pi, pi, nx)
     ky = np.linspace(-pi, pi, ny)
     kz = np.linspace(-pi, pi, nz)
 
+    # Return a meshgrid
     return np.meshgrid(kx, ky, kz, indexing='ij')
 
 
