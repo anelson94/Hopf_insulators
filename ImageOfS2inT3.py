046894b55f9d20ef037bbf218ae35a74c5c770fc
alexandra <alexsandranelsn94@gmail.com>
2020-02-12 18:03:51 +0100

    Change mayavi plotting from contours to scatter. Modify preimage function to use with different models and different parameters. For edge-constant model remove edge points for clarity.

--- ImageOfS2inT3.py
+++ ImageOfS2inT3.py
@@ -27,7 +27,7 @@ def color_list():
     return (red, green, blue, brown, lightblue)
 
 
-def preimage(n, model, eps1=0.1, eps2=0.1):
+def preimage(n, model, eps1, eps2):
     """Preimage at T^3 of a point n at S^2 for a map model"""
 
     im = (np.isclose(model['hx'], n[0], atol=eps1, rtol=eps2)
@@ -37,29 +37,53 @@ def preimage(n, model, eps1=0.1, eps2=0.1):
     return im
 
 
-def preimage_plot(model, *n_list, nx=101, ny=101, nz=101, **kwargs):
+def preimage_plot(model, *n_list, nx=101, ny=101, nz=101, eps1=0.1, eps2=0.1,
+                  **kwargs):
     """Plot preimages of n_list points"""
     kx, ky, kz = hopfham.mesh_make(nx, ny, nz)
-    if callable(model):
-        model = model(kx, ky, kz)
+    if (model == hopfham.model_edgeconst
+        or model == hopfham.model_edgeconst_maps
+            or model == hopfham.model_edgeconst_maps_rotated):
 
-    mlab.figure(bgcolor=(1, 1, 1))
+        kx = kx[2:-2, 2:-2, 2:-2]
+        ky = ky[2:-2, 2:-2, 2:-2]
+        kz = kz[2:-2, 2:-2, 2:-2]
+    if callable(model):
+        model = model(kx, ky, kz, **kwargs)
+
+    mlab.figure(bgcolor=(1, 1, 1), fgcolor=(0., 0., 0.))
     i_color = 0
     colors = color_list()
     for n in n_list:
-        im = preimage(n, model, **kwargs)
-        con = mlab.contour3d(im, contours=2,
-                             color=colors[i_color % len(colors)],
-                             transparent=False)
+        im = preimage(n, model, eps1, eps2)
+        coords = np.where(im == 1)
+        print(len(coords))
+        # con = mlab.contour3d(kx, ky, kz, im, contours=2,
+        #                      color=colors[i_color % len(colors)],
+        #                      transparent=False)
+        x_coord = coords[0]*2*pi/(nx-1) - pi
+        y_coord = coords[1]*2*pi/(ny-1) - pi
+        z_coord = coords[2]*2*pi/(nz-1) - pi
+
+        con = mlab.points3d(x_coord, y_coord, z_coord, scale_factor=.05,
+                            color=colors[i_color % len(colors)])
         i_color += 1
-    mlab.axes(con, xlabel='x', ylabel='y', zlabel='z', color=(0, 0, 0))
+    points = np.array([-pi, pi])
+    pointsx, pointsy, pointsz = np.meshgrid(points, points, points,
+                                            indexing='ij')
+    pointsx = np.ravel(pointsx)
+    pointsy = np.ravel(pointsy)
+    pointsz = np.ravel(pointsz)
+    mlab.points3d(pointsx, pointsy, pointsz, scale_factor=.25)
+    # mlab.axes(con, xlabel='x', ylabel='y', zlabel='z', color=(0, 0, 0),
+    #           extend=[-pi, pi, -pi, pi, -pi, pi])
     mlab.show()
 
 
-def image_plot(kx, ky, kz, model):
+def image_plot(kx, ky, kz, model, **kwargs):
     """Find """
     if callable(model):
-        model = model(kx, ky, kz)
+        model = model(kx, ky, kz, **kwargs)
 
     # Plot images of (kx, ky, kz)
     fig = plt.figure()
@@ -78,25 +102,54 @@ def main():
 
     if preimageplot == 1:
         # Three points in S2 (3d vectors with unitary norms)
-        n = np.array([-0.3696628, 0, -0.92916596])
+        # n = np.array([-0.3696628, 0, -0.92916596])
         # n = np.array([-0.459850, 0.136869, -0.8773850])
-        n1 = np.array([1, 0, 0])
-        # n2 = np.array([0, 1, 0])
+        a = 3*pi/4
+        n = np.array([np.sin(a), 0, -np.cos(a)])
+        n1 = np.array([0, 1, 0])
+        # n1 = np.array([np.sqrt(3)/2, 1/2, 0])
+        # n = np.array([-0.45985, 0.136869, -0.877385])
+        # n2 = np.array([0, 0, -1])
         # n3 = np.array([0, 0, 1])
+        # n4 = np.array([1, 0, 0])
+        # n5 = np.array([np.sqrt(2)/2, 0, np.sqrt(2)/2])
         # n4 = np.array([-1, 0, 0])
         # n5 = np.array([0, -1, 0])
 
-        preimage_plot(hopfham.model_mrw_norm, n, n1, nx=401, ny=401, nz=401,
-                      eps1=0.05, eps2=0.05)
+        # List of models
+        # hopfham.model_mrw
+        # hopfham.model_mrw_norm
+        # hopfham.model_edgeconst
+        # hopfham.model_mrw_maps
+        # hopfham.model_mrw_maps_rotated
+        # hopfham.model_edgeconst_maps
+        # hopfham.model_edgeconst_maps_rotated
+
+        # MRW model args
+        # model_args = {'m': 1}
+        # No args needed for model
+        # model_args = {}
+        # Rotated MRW model from maps args
+        # model_args = {'m': 1, 'alpha': 0}
+        # Rotation angle in args
+        model_args = {'alpha': -a/2}
+
+        preimage_plot(hopfham.model_mrw_maps_rotated, n, n1,
+                      nx=201, ny=201, nz=201,
+                      eps1=0.05, eps2=0.05, **model_args)
 
     if imageplot == 1:
         # kx = np.ones(100) * pi
         # ky = np.ones(100) * 2.1
         # kz = np.linspace(-pi, pi, 100)
+        model_args = {'alpha': pi/8}
+        # model_args = {}
         kx = pi
-        ky = 2.1
-        kz = -1.3
-        hx, hy, hz = image_plot(kx, ky, kz, hopfham.model_mrw_norm)
+        ky = 0
+        kz = 0
+        hx, hy, hz = image_plot(kx, ky, kz,
+                                hopfham.model_edgeconst_maps_rotated,
+                                **model_args)
         print(hx, hy, hz)
 
 
